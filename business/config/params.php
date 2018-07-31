ce6514b539f3584a442f366fb2285e198d6987d8
tianweimin <tianweimin@ztgame.com>
2018-07-31 09:27:32 +0800

    fix bug

--- business/config/params.php
+++ business/config/params.php
@@ -12,7 +12,10 @@ $params = [
     'modules' => [//外部接入系统
         'gamm3' => [
             'gateWay' => 'http://gamm3.dev.ztgame.com'
-        ]
+        ],
+        'risk' => [
+            'gateWay' => 'http://gamm3.dev.ztgame.com/fdsf/fdsf',
+        ],
     ],
     'menus_ext' => [//菜单维度扩展
         1 => '征途',
