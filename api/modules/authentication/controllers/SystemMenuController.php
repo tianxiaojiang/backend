1d68c3180288f945a0745b9c70af1ae67c58332a
tianweimin <tianweimin@ztgame.com>
2018-11-26 12:05:20 +0800

    add function of select privilege

--- api/modules/authentication/controllers/SystemMenuController.php
+++ api/modules/authentication/controllers/SystemMenuController.php
@@ -4,7 +4,9 @@ namespace Api\modules\authentication\controllers;
 
 use Backend\helpers\Helpers;
 use Backend\modules\admin\models\SystemMenu;
+use Backend\modules\admin\models\SystemPriv;
 use Backend\modules\common\controllers\JwtController;
+use yii\helpers\ArrayHelper;
 
 /**
  * Created by PhpStorm.
@@ -30,5 +32,31 @@ class SystemMenuController extends JwtController
         }
 
         return $menus;
+    }
+
+    //获取所有的权限列表
+    public function actionPrivileges()
+    {
+        $isMaintain = intval(Helpers::getRequestParam('isMaintain'));
+        $requestActions = array_flip(explode(',', trim(Helpers::getRequestParam('actions'))));
+
+        //维护后台的权限列表
+        $privilegeType = $isMaintain ? SystemPriv::PRIVILEGE_TYPE_SETTING : SystemPriv::PRIVILEGE_TYPE_BUSINESS;
+        $privileges = \Yii::$app->user->identity->getPrivilege($privilegeType);
+        $privilegesValue = array_map(function ($col) {
+            return $item = '/' . $col['sp_module'] . '/' . $col['sp_controller'] . '/' . $col['sp_action'];
+        }, $privileges);
+
+        foreach ($requestActions as $requestAction => $val) {
+            $requestActions[$requestAction] = in_array($requestAction, $privilegesValue);
+        }
+
+        $privilegesRes = ["privileges" => $requestActions];
+        if($isMaintain < 1) {//业务后台
+            $callback = Helpers::getRequestParam('callback');
+            echo $callback . '(' . json_encode(['code' => 0, 'msg' => '', 'data' => $privilegesRes]) . ')';exit;
+        }
+
+        return $privilegesRes;
     }
 }
