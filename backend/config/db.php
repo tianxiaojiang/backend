bdfe1b7f8fde562b3d57777f9b7fd8e29ee8d675
tianweimin <tianweimin@ztgame.com>
2018-11-27 15:01:41 +0800

    switch online config

--- backend/config/db.php
+++ backend/config/db.php
@@ -9,6 +9,14 @@
 use yii\helpers\ArrayHelper;
 
 $db = [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=10.20.9.117;port=3306;dbname=integration_background',
+    'username' => 'giant_manage',
+    'password' => 'qGwQAZBR5j4DJYyI',
+    'charset' => 'utf8mb4',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
 ];
 
 return file_exists(__DIR__ . '/db-local.php') ? ArrayHelper::merge($db, require_once(__DIR__ . '/db-local.php')) : $db;
--- backend/config/redis.php
+++ backend/config/redis.php
@@ -9,7 +9,10 @@
 use yii\helpers\ArrayHelper;
 
 $redis = [
-
+    'class' => 'yii\redis\Connection',
+    'hostname' => '10.20.9.46',
+    'port' => 6666,
+    'database' => 0,
 ];
 
 return file_exists(__DIR__ . '/redis-local.php') ? ArrayHelper::merge($redis, require_once(__DIR__ . '/redis-local.php')) : $redis;
