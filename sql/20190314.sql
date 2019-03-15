42b6ba5d45f6f864bcdb86115dca8b540f64acfc
tianweimin <tianweimin@ztgame.com>
2019-03-15 16:09:36 +0800

    fix bug

--- sql/20190314.sql
+++ sql/20190314.sql
@@ -7,6 +7,3 @@ alter table `s2_system_group` add `kind` tinyint(1) not null default 0 comment "
 alter table `s3_system_group` add `kind` tinyint(1) not null default 0 comment "角色类型 0,普通角色 1,独有角色" after `privilege_level`;
 alter table `s4_system_group` add `kind` tinyint(1) not null default 0 comment "角色类型 0,普通角色 1,独有角色" after `privilege_level`;
 alter table `s5_system_group` add `kind` tinyint(1) not null default 0 comment "角色类型 0,普通角色 1,独有角色" after `privilege_level`;
-alter table `s6_system_group` add `kind` tinyint(1) not null default 0 comment "角色类型 0,普通角色 1,独有角色" after `privilege_level`;
-alter table `s7_system_group` add `kind` tinyint(1) not null default 0 comment "角色类型 0,普通角色 1,独有角色" after `privilege_level`;
-alter table `s8_system_group` add `kind` tinyint(1) not null default 0 comment "角色类型 0,普通角色 1,独有角色" after `privilege_level`;
